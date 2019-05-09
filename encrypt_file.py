73988c90349e0c5dcb746fd756d380f7a9972af6
ryanusHolmanus <46622886+ryanusHolmanus@users.noreply.github.com>
2019-05-08 21:33:46 -0500

    Update encrypt_file.py

--- encrypt_file.py
+++ encrypt_file.py
@@ -1,12 +1,27 @@
-import cryptography
+from cryptography.fernet import Fernet
 import sys
 
+key = Fernet.generate_key()
 
-def dec_file(fn):
+#key = b'0xAA'
+fernet = Fernet(key)
+
+def akey():
+	file = open('key.key', 'wb')
+	file.write(key) # The key is type bytes still
+	file.close()
+	return
+
+def dec_file():
+    with open(sys.argv[2], 'rb') as f:
+        data = f.read()
+    fernet = Fernet(key)
+    encrypted = fernet.decrypt(data)
+    with open(sys.argv[3], 'wb') as f:
+        f.write(encrypted)
     return
 
 def enc_file(data):
-    fernet = Fernet(key)
     encr=fernet.encrypt(data)
     outf=sys.argv[2]
     with open(outf,'wb') as oof:
@@ -22,10 +37,15 @@ def load_input(fn):
     return data
 
 def main():
-    key = bb''
+    akey()
     fn = sys.argv[1]
+    print("loading input file")
     data=load_input(fn)
+    print('encrypting file.')
     enc_file(data)
+    print('decrypting file')
+    dec_file()
+    print('finished')
     return
 
 main();
